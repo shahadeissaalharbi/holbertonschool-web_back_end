5a474051133b2e0076eeca43fe0f3e77a8b3f6eb
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-08-25 10:47:31 +0000

    3-to_str: type-annotated to_str function

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that provides a type-annotated float-to-string function."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of the given float."""
+    return str(n)
