5397a2f2b1956e5415e174dc82dbec1b467a76b4
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-09-13 14:46:02 +0200

    Fix pycodestyle indentation issue

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -39,8 +39,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
-                         page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
         Return a dictionary containing pagination data that is
         resilient to deletions in the dataset.
