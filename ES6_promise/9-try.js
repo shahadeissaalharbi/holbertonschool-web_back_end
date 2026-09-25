c6cfcafd44ffa7aadc76afe1a863ceb821e4f585
shahadeissaalharbi <shahadeissa111@gmail.com>
2026-09-25 19:17:20 +0000

    Implement guardrail function using try catch and finally

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (err) {
+    queue.push(String(err));
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
